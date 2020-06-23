358d3bcf251c62a6b5e3dde91a3f817b19c07e05
Soul <sornwilliam@gmail.com>
2020-06-23 16:49:42 -0500

    added inquirer dbStart

--- app.js
+++ app.js
@@ -16,6 +16,67 @@ const connection = mysql.createConnection({
 
 connection.connect(function (err) {
     if (err) throw err;
-    console.log("Connected as id " + connection.threadId);
-
+    
+    dbStart();
 });
+
+function dbStart(){
+    inquirer
+        .prompt({
+            name:"menu",
+            type:"list",
+            message:"Welcome to Employee DB. \n Here you will be able to search/add employees, departments, or roles.\n What would you like to do?",
+            choices: [
+                "View departments",
+                "View employees",
+                "View roles",
+                "Add departments",
+                "Add employees",
+                "Add roles",
+                "Exit"
+            ]
+        })
+        .then(function(answer) {
+            switch (answer.menu) {
+                case "View departments":
+                    //View via dep
+                    console.log("Department");
+                    break;
+                
+                case "View employees":
+                    //view via employees
+                    console.log("Employees");
+                    break;
+
+                case "View roles":
+                    //view via roles
+                    console.log("Roles");
+                    break;
+                case "Add departments":
+                    //add departments
+                    console.log("Add Department");
+                    break;
+
+                case "Add employees":
+                    //add employees
+                    console.log("Add Employee");
+                    break;
+
+                case "Add roles":
+                    console.log("Add roles");
+                    break;
+
+                case "Exit":
+                    connection.end();
+                    break;
+            };
+        });
+};
+
+function depSearch(){
+    inquirer
+        .prompt({
+            name:"department",
+            type:""
+        })
+}
